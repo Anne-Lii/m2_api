4811fa705825896d8d2a2b271ba034e1293aee79
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-01-30 20:14:05 +0100

    ändrade filnamn från index.js till server.js. la till .env fil med miljövariabler till databasen MongoDB, la till databas anslutning med mongoose till MongoDB

---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+//includes
+const Hapi = require('@hapi/hapi'); //require hapi
+require('dotenv').config();// Get environment variables from the .env file
+const mongoose = require('mongoose');// Imports Mongoose library 
+
+const init = async () => {
+
+    // Creates a new Hapi server instance with CORS enabled
+    const server = Hapi.server({
+        port: process.env.PORT || 5000,
+        host: 'localhost', 
+        routes: {
+            cors: {
+                origin: ['*'], // Allow all origins.
+            }
+        },
+        
+    });
+
+    //Connect to MongoDB database
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    }).catch((error) => {
+        console.error('Could not connect to MongoDB', error);
+    });
+
+    //Routes
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    //starting server
+    await server.start();
+    console.log('Server running on: ', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+//Call the init function to start the server
+init(); 
