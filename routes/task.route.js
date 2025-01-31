c2c5a525d4395d891962c2d14a64da302703695d
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-01-31 10:37:53 +0100

    justerat kommentarer

--- routes/task.route.js
+++ routes/task.route.js
@@ -4,30 +4,30 @@
 const TaskController = require('../controllers/task.controller');//Controller
 
 module.exports = [
-    {
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
+    {   //GET route for root '/' and returns a Welcome message
+        method: 'GET',//HTTP method
+        path: '/',//URL path
+        handler: (request, h) => {//function that handles the request
 
-            return 'Välkommen till Anne-Liis API!';
+            return 'Välkommen till Anne-Liis API!';//response to klient
         }
     },
-    {
+    {   //GET route to get all tasks
         method: 'GET',
         path: '/task',
-        handler: TaskController.getAllTasks
+        handler: TaskController.getAllTasks //calls the getAllTasks function in the controller
     },
-    {
+    {   //POST route to create new tasks
         method: 'POST',
         path: '/task',
         handler: TaskController.createNewTask
     },
-    {
+    {   //DELETE route to delete a task by id
         method: 'DELETE',
         path: '/task/{id}',
         handler: TaskController.deleteTask
     },
-    {
+    {   //PUT route to update a task by id
         method: 'PUT',
         path: '/task/{id}',
         handler: TaskController.updateTask
